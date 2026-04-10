d844b82a7e8b4869f531f7d1cd868d0c29177e0b
JudeOmohe <ST10535101@myemeris.edu.za>
2026-04-10 16:19:08 +0200

    commit 1

---
+++ src/main/java/com/mycompany/poe1/NewClass.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.poe1;
+
+public class NewClass {
+    private String firstName;
+    private String lastName;
+    
+    
+    public NewClass(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/com/mycompany/poe1/POE1.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.poe1;
+
+import java.util.Scanner;
+
+public class POE1 {
+
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+  
+        
+        System.out.println("=== ACCOUNT REGISTRATION ===");
+        
+        System.out.println("\nEnter first name:");
+        String firstName = input.nextLine();
+        System.out.println("\nEnter last name: ");
+        String lastName = input.nextLine();
+    }
+}
